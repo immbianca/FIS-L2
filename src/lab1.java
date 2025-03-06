7360c16d380f27b752e06a76522e6802ffb2c745
immbianca <bianca.mustea@student.upt.ro>
2025-03-06 11:25:03 +0200

    rng

--- src/lab1.java
+++ src/lab1.java
@@ -4,27 +4,42 @@ class Ajutor {
     void afisare() {
         System.out.println("Hello world");
     }
-    
+
     void info() {
-        System.out.println("FIS");
-        System.out.println("UPT AC");
-        System.out.println("Mustea Bianca-Maria");
+        System.out.println("Nume proiect: Proiect Java");
+        System.out.println("Facultate: Universitatea XYZ");
+        System.out.println("Autor: Alex Pescaru");
     }
 }
 
-public class lab1 {
+class Test {
+    private int rng;
+
+    public Test() {
+        this.rng = (int) (Math.random() * Integer.MAX_VALUE);
+    }
+
+    public int getRng(int x) {
+        return rng % x;
+    }
+}
+
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Ajutor ajutor = new Ajutor();
-        
+        Test test = new Test();
+
         while (true) {
             System.out.print("Introduceți un șir de caractere: ");
             String input = scanner.nextLine();
-            
+
             if (input.equals("hello")) {
                 ajutor.afisare();
             } else if (input.equals("info")) {
                 ajutor.info();
+            } else if (input.equals("num")) {
+                System.out.println("Valoarea rng %% 5: " + test.getRng(5));
             } else if (input.equals("exit")) {
                 System.out.println("Program terminat.");
                 break;
@@ -32,7 +47,7 @@ public class lab1 {
                 System.out.println("Ați introdus: " + input);
             }
         }
-        
+
         scanner.close();
     }
 }
